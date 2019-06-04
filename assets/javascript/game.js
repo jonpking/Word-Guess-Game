9b395cc8daeef8e31ae59bf8486c707aa7038d67
Enigma <jonpking@gmail.com>
2019-06-03 21:06:51 -0700

    added win/loss checks, output, and reset and prevented duplicate guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,14 @@
 
 // variable declarations, random word selection, and splitting of word into letters
 
-const words = ["Aquamarine", "Burgundy", "Periwinkle", "Goldenrod", "Emerald", "Fuchsia"];
-const selectedWord = words[Math.floor(Math.random()*words.length)]
+const words = ["aquamarine", "burgundy", "periwinkle", "goldenrod", "emerald", "fuchsia"];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 const selectedWordArray = selectedWord.split("");
 const blanks = selectedWordArray.slice(0).fill("_");
 let guessesRemaining = 10;
-const lettersGuessed = [];
+let lettersGuessed = [];
 let wins = 0;
 let losses = 0;
-
-
-console.log("Selected Character")
-console.log(selectedWord);
-console.log("Length and Split")
-console.log(selectedWordArray);
-console.log(blanks);
 
 // initial visual elements
 
@@ -26,34 +19,42 @@ document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
 document.getElementById("currentWord").innerHTML = blanks;
 
 // registers key press and reduces number of guesses remaining, if out of guesses remaining it increases loses, 
-// adds letters to list of letters guessed
-
-document.addEventListener("keyup", function(event){
-    lettersGuessed.push(event.key);
-    document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
-    guessesRemaining--;
-    document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
-    if(guessesRemaining < 1){
-        losses++;
-        document.getElementById("losses").innerHTML = losses;
-    }
-
+// adds letters to list of letters guessed, checks if letter has already been guessed and prevents duplication,
 // checks if key pressed exists in selected word, if it does it replaces "_" with letter key pressed
 
-    selectedWordArray.forEach(function(letter, index){
-        console.log("key press", event.key);
-        console.log("letter", letter);
-        if(event.key === letter){
+document.addEventListener("keyup", function (event) {
+    if (lettersGuessed.includes(event.key)) {
+        return;
+    }
+    selectedWordArray.forEach(function (letter, index) {
+        if (event.key === letter) {
             blanks[index] = letter;
-            console.log("key press", event.key);
         };
     });
-
-// checks if entire word has been guessed, iterates wins if it has
-
-
-    console.log("blanks", blanks);
-    console.log("guessesRemaining", guessesRemaining);
-    console.log("lettersGuessed", lettersGuessed);
+    lettersGuessed.push(event.key);
+    document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+    if (blanks.toString() == selectedWordArray.toString()) {
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        alert("YOU WIN!");
+        lettersGuessed = [];
+        guessesRemaining = 10;
+        selectedWord = words[Math.floor(Math.random() * words.length)];
+        selectedWordArray = selectedWord.split("");
+        blanks = selectedWordArray.slice(0).fill("_");
+        return;
+    }
+    guessesRemaining--;
+    document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+    if (guessesRemaining < 1) {
+        losses++;
+        document.getElementById("losses").innerHTML = losses;
+        alert("YOU LOSE!");
+        lettersGuessed = [];
+        guessesRemaining = 10;
+        selectedWord = words[Math.floor(Math.random() * words.length)];
+        selectedWordArray = selectedWord.split("");
+        blanks = selectedWordArray.slice(0).fill("_");
+        return;
+    }
 });
-
