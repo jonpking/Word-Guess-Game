bac6ad876cbd889bb83ab29b7f68049fd0536d56
Enigma <jonpking@gmail.com>
2019-06-05 19:18:38 -0700

    fixed current word to not display commas, game now resets on win/lose after alert, updated several variable types and bug fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,8 @@
 
 const words = ["aquamarine", "burgundy", "periwinkle", "goldenrod", "emerald", "fuchsia"];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
-const selectedWordArray = selectedWord.split("");
-const blanks = selectedWordArray.slice(0).fill("_");
+let selectedWordArray = selectedWord.split("");
+let blanks = selectedWordArray.slice(0).fill("_");
 let guessesRemaining = 10;
 let lettersGuessed = [];
 let wins = 0;
@@ -16,13 +16,16 @@ document.getElementById("wins").innerHTML = wins;
 document.getElementById("losses").innerHTML = losses;
 document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
 document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
-document.getElementById("currentWord").innerHTML = blanks;
+document.getElementById("currentWord").innerHTML = blanks.join(" ");
 
 // registers key press and reduces number of guesses remaining, if out of guesses remaining it increases loses, 
 // adds letters to list of letters guessed, checks if letter has already been guessed and prevents duplication,
 // checks if key pressed exists in selected word, if it does it replaces "_" with letter key pressed
 
 document.addEventListener("keyup", function (event) {
+    if (event.keyCode < 65 || event.keyCode > 90) {
+        return;
+    }
     if (lettersGuessed.includes(event.key)) {
         return;
     }
@@ -32,16 +35,20 @@ document.addEventListener("keyup", function (event) {
         };
     });
     lettersGuessed.push(event.key);
+    document.getElementById("currentWord").innerHTML = blanks.join(" ");
     document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
     if (blanks.toString() == selectedWordArray.toString()) {
         wins++;
         document.getElementById("wins").innerHTML = wins;
-        alert("YOU WIN!");
+        setTimeout(function () { alert("YOU WIN!") }, 1);
         lettersGuessed = [];
+        document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
         guessesRemaining = 10;
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
         selectedWord = words[Math.floor(Math.random() * words.length)];
         selectedWordArray = selectedWord.split("");
         blanks = selectedWordArray.slice(0).fill("_");
+        document.getElementById("currentWord").innerHTML = blanks.join(" ");
         return;
     }
     guessesRemaining--;
@@ -49,12 +56,15 @@ document.addEventListener("keyup", function (event) {
     if (guessesRemaining < 1) {
         losses++;
         document.getElementById("losses").innerHTML = losses;
-        alert("YOU LOSE!");
+        setTimeout(function () { alert("YOU LOSE") }, 1);
         lettersGuessed = [];
+        document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
         guessesRemaining = 10;
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
         selectedWord = words[Math.floor(Math.random() * words.length)];
         selectedWordArray = selectedWord.split("");
         blanks = selectedWordArray.slice(0).fill("_");
+        document.getElementById("currentWord").innerHTML = blanks.join(" ");
         return;
     }
 });
